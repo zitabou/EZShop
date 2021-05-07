21ea2e9687bfbb880789a5cede8b392e7271e181
Zissis Tabouras <s284685@studenti.polito.it>
2021-05-07 12:08:49 +0300

    update DAO,Shop

---
+++ src28/main/java/it/polito/ezshop/DAO/CustomerDAO.java
@@ -0,0 +1,24 @@
+package it.polito.ezshop.DAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import it.polito.ezshop.classes.ezCustomer;
+import it.polito.ezshop.exceptions.*;
+
+public class CustomerDAO {
+
+/*	
+	public static void Create(Customer c)} throws DAOexception, InvalidCustomerIdException {
+		Connection conn = DBManager.getConnection();	
+		try {
+			
+			
+			
+			
+			
+		}catch(SQLException e) {
+			throw new DAOexception("something went wrong when creating customer " +c.getId());
+		}
+	}*/
+}
