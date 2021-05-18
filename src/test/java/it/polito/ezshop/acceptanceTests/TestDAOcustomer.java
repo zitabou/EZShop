ba8b1c23bc4de277f61ee8a1483bda9980e259e5
@s289832 <s289832@studenti.polito.it>
2021-05-18 15:25:47 +0200

    feat: add testing for apdate delete read

--- src/test/java/it/polito/ezshop/acceptanceTests/TestDAOcustomer.java
+++ src/test/java/it/polito/ezshop/acceptanceTests/TestDAOcustomer.java
@@ -7,6 +7,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,11 +16,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import it.polito.ezshop.classes.ezCustomer;
-import it.polito.ezshop.classesDAO.DAObalanceOperation;
 import it.polito.ezshop.classesDAO.DAOcustomer;
 import it.polito.ezshop.classesDAO.DAOexception;
+import it.polito.ezshop.classesDAO.DAOproductType;
 import it.polito.ezshop.classesDAO.DBManager;
 import it.polito.ezshop.data.Customer;
+import it.polito.ezshop.data.ProductType;
 
 
 public class TestDAOcustomer {
@@ -44,20 +47,70 @@ public class TestDAOcustomer {
 		cus3.setId(DAOcustomer.Create(cus3));
 		cus4 = new ezCustomer("testcus4", 4, "testcard4", 4);
 		cus4.setId(DAOcustomer.Create(cus4));
-		
-		
+				
 	}
 	
     @Test
-    public void testCreateCustomer() {
-    	
+    public void testCreateCustomer() {  	
     	Customer cus5 = new ezCustomer("testcus5", 5, "testcard5", 5);
-    	int outcome;
-    	
+    	int outcome;	
 		outcome = DAOcustomer.Create(cus5);
-		Assert.assertEquals(5, outcome);
-    	
+		Assert.assertEquals(5, outcome);  	
     }
+    
+
+  	@Test
+  	public void testReadCustomerById() {
+  		
+  		Customer cus = DAOcustomer.Read(cus3.getId());
+  		Assert.assertTrue(cus.getCustomerName().equals("testcus3"));
+  	}
+    
+  	
+  	@Test
+  	public void testReadCustomerByWrongId() {
+  		
+  		Customer cus = DAOcustomer.Read(10);
+  		Assert.assertNull(cus);
+  	}
+  	
+ // ALL	
+ 	@Test
+ 	public void testReadAllCustomer() {
+ 		
+ 		List<Customer> custs = new ArrayList<Customer>(DAOcustomer.readAll().values());
+ 		
+ 		Assert.assertEquals(4, custs.size());
+ 		Assert.assertTrue(custs.get(0).getCustomerName().equals("testcus1"));
+ 		Assert.assertTrue(custs.get(1).getCustomerName().equals("testcus2"));
+ 	}
+ 	
+ 	//Update customer	
+ // by ID
+ 	@Test
+ 	public void testUpdateCustomer() {
+ 		
+ 		cus4.setCustomerName("testcus4_Updated");
+ 		DAOcustomer.Update(cus4);
+ 		
+ 		
+ 		cus4 = DAOcustomer.Read(cus4.getId());
+ 		
+ 		Assert.assertTrue(cus4.getCustomerName().equals("testcus4_Updated"));
+ 	}
+ 	
+ 	@Test
+ 	public void testDeleteCustomer() {
+ 		DAOcustomer.Delete(4);
+ 		Assert.assertEquals(3,DAOcustomer.readAll().size());
+ 		Assert.assertNull(DAOcustomer.Read(cus4.getId()));
+ 	}
+ 	
+	@Test
+ 	public void testDeleteAllCustomer() {
+ 		DAOcustomer.DeleteAll();
+ 		Assert.assertEquals(0,DAOcustomer.readAll().size());
+ 	}
 
 
 }
