92cdbce88969f589e26c0a2db18ddc6917e9b5c1
Zissis Tabouras <s284685@studenti.polito.it>
2021-06-06 14:54:44 +0300

    add Product constructor

--- src/main/java/it/polito/ezshop/classes/Product.java
+++ src/main/java/it/polito/ezshop/classes/Product.java
@@ -2,9 +2,19 @@ package it.polito.ezshop.classes;
 
 public class Product {
 	
+	public Product() {
+		RFID = "";
+		barCode = "";
+	}
+
+	public Product(String RFID, String barCode) {
+		this.RFID = RFID;
+		this.barCode = barCode;
+	}
+	
 	public String getRFID() {return RFID;}
 
-	public void setRFID(String rFID) {RFID = rFID;}
+	public void setRFID(String RFID) {this.RFID = RFID;}
 	
 	public String getBarCode() {return barCode;}
 
